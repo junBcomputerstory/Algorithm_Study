e032db7dc078ad7707c6890bec2799acdeafa0bf
Hongjun <ggaye159@naver.com>
2022-12-08 19:01:16 +0900

    백준 DP 실버3

---
+++ 백준/BJ_2839.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class BJ_2839 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int cnt=0;
+        while(true){
+            if(n%5==0){
+                cnt+=n/5;
+                System.out.println(cnt);
+                break;
+            }
+            else if(n<0){
+                System.out.println("-1");
+                break;
+            }
+            else{
+                n-=3;
+                cnt++;
+            }
+        }
+    }
+}
