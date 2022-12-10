8f46170aa36d6b7d513a55df856e29935ba535bc
Hongjun <ggaye159@naver.com>
2022-12-10 17:35:19 +0900

    백준 DP 실버3

---
+++ 백준/BJ_9095.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class BJ_9095 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        int answer[]=new int[t];
+        int n=0;
+        int pre[]={1,2,4};
+        for(int i=0;i<t;i++){
+            n=Integer.parseInt(br.readLine());
+            if(n<4)
+                answer[i]=pre[n-1];
+            else
+                answer[i]=plus(n);
+        }
+        for(int i=0;i<t;i++)
+            System.out.println(answer[i]);
+    }
+    public static int plus(int n){
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=4;
+        for(int i=4;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+}
