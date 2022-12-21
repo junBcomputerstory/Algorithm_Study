92ab46a4691bcaba5fb554fec73135c3a951a259
Hongjun <ggaye159@naver.com>
2022-12-21 18:29:28 +0900

    백준 DataST 실버4

--- 백준/BJ_11726.java
+++ 백준/BJ_11726.java
@@ -4,9 +4,9 @@ public class BJ_11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long dp[] = new long[n + 1];
+        int dp[] = new int[n + 1];
         dp[1] = 1;
-        if (n > 2) {
+        if (n >=2) {
             dp[2] = 2;
             for (int i = 3; i < n + 1; i++) {
                 dp[i] = (dp[i - 1] + dp[i - 2])%10007;
